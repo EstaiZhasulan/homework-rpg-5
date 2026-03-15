13d37db970cb8e5e388d872d6b739204b61a02ee
SaoMadao <zhassulan.estai@narxoz.kz>
2026-03-15 23:50:46 +0500

    implemented AdventureResult

--- src/com/narxoz/rpg/facade/AdventureResult.java
+++ src/com/narxoz/rpg/facade/AdventureResult.java
@@ -4,11 +4,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class AdventureResult {
+    private String heroName;
+    private String bossName;
+
     private String winner;
     private int rounds;
     private String reward;
     private final List<String> log = new ArrayList<>();
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getBossName() {
+        return bossName;
+    }
+
+    public void setBossName(String bossName) {
+        this.bossName = bossName;
+    }
 
     public String getWinner() {
         return winner;
