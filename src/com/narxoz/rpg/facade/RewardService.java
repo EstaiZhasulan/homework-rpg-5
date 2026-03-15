b63a5fe7c96c0c804161eb46f6f1cfc99436030d
SaoMadao <zhassulan.estai@narxoz.kz>
2026-03-16 02:56:42 +0500

    implemente RewardService

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -1,11 +1,34 @@
 package com.narxoz.rpg.facade;
 
+
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward (invalid result)";
         }
-        return "TODO";
+        if (battleResult.getWinner() == null) {
+            return "No reward (unknown winner)";
+        }
+
+        String heroName = battleResult.getHeroName();
+        String winner = battleResult.getWinner();
+
+        if ("Draw".equalsIgnoreCase(winner)) {
+            return "No reward (draw)";
+        }
+
+        boolean heroWon = heroName != null && heroName.equals(winner);
+        if (!heroWon) {
+            return "Consolation: 5 gold";
+        }
+
+        int rounds = battleResult.getRounds();
+        if (rounds <= 3) {
+            return "Legendary chest: 120 gold";
+        }
+        if (rounds <= 6) {
+            return "Rare chest: 60 gold";
+        }
+        return "Common chest: 30 gold";
     }
 }
